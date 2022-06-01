0d9620707da34efea6b3a526c8c88dfdb176f2db
Jasmine Mounes <73711236+warm-wyvern@users.noreply.github.com>
2022-05-31 22:56:53 -0700

    hello, running sum of array

---
+++ RunningSum1D.java
@@ -0,0 +1,10 @@
+public class RunningSum1D {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+    
+    public int[] runningSum(int[] nums) {
+        int[] o = new int[10];
+        return o;
+    }
+}
